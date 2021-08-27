716585ff9599ee7d2f453dab2ad556fae5515790
Ooohstream <69838796+Ooohstream@users.noreply.github.com>
2021-08-27 18:02:23 +0300

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png"
           alt="spotify.logo"
         />
-        <a href="http://192.168.1.65:5000/login">Become a host</a>
+        <a href="http://partyfy.xyz/login">Become a host</a>
       </div>
     </header>
   );
