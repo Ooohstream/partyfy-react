d03aa48142aeb8837acab2e3e730d79bcd20420c
Ooohstream <69838796+Ooohstream@users.noreply.github.com>
2021-08-27 17:43:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* express installation */
 
 const express = require("express");
+const path = require("path");
 const app = express();
 
 /* Body middleware */
@@ -48,7 +49,7 @@ function millisToMinutesAndSeconds(millis) {
 
 /* REACT connection */
 
-app.use(express.static("client\\build"));
+app.use(express.static(path.join(__dirname, "client", "build"))));
 
 /* OR */
 
